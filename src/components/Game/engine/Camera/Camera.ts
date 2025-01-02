483d534a5bd8b20d442df4fa063c0a673b6ef2d9
matveygrevtsov <matveygrevcov@gmail.com>
2025-01-02 14:41:26 +0300

    add camera smooth rotation

--- src/components/Game/engine/Camera/Camera.ts
+++ src/components/Game/engine/Camera/Camera.ts
@@ -18,8 +18,16 @@ import {
   MIN_RADIUS,
   SHOW_TARGET,
   CANVAS_PADDING_PX,
+  ROTATION_ANIMATION_DURATION_MS,
+  ALPHA_DELTA,
 } from "./constants";
 import { GROUND_HEIGHT, GROUND_WIDTH } from "../GameController/constants";
+
+interface IRotationAnimation {
+  startTimeMs: number;
+  startAngle: number;
+  finishAngle: number;
+}
 
 interface IProps {
   canvas: HTMLCanvasElement;
@@ -33,6 +41,7 @@ export class Camera {
   private readonly target: Mesh;
   private readonly movementVector: Vector3;
   private rotationGradationIndex: number;
+  private rotationAnimationStartTimeMs?: number;
 
   constructor({ canvas, scene }: IProps) {
     this.canvas = canvas;
@@ -52,7 +61,7 @@ export class Camera {
       BETA_ROTATION_ANGLE,
       DEFAULT_RADIUS,
       Vector3.Zero(),
-      scene,
+      scene
     );
     camera.attachControl(canvas, true);
     camera.lowerRadiusLimit = MIN_RADIUS;
@@ -71,6 +80,38 @@ export class Camera {
     }
     camera.setTarget(target);
     return target;
+  }
+
+  private refreshTargetPositionByMovementVector() {
+    const { scene, target, movementVector, rotationAnimationStartTimeMs } =
+      this;
+    if (
+      !movementVector.length() ||
+      !scene.deltaTime ||
+      rotationAnimationStartTimeMs !== undefined
+    ) {
+      return;
+    }
+    const speed = (scene.deltaTime / 1000) * 4;
+    const groundHalfWidth = GROUND_WIDTH / 2;
+    const groundHalfHeight = GROUND_HEIGHT / 2;
+    target.position.addInPlace(movementVector.scale(speed));
+
+    if (target.position.x > groundHalfWidth) {
+      target.position.x = groundHalfWidth;
+    }
+
+    if (target.position.x < -groundHalfWidth) {
+      target.position.x = -groundHalfWidth;
+    }
+
+    if (target.position.z > groundHalfHeight) {
+      target.position.z = groundHalfHeight;
+    }
+
+    if (target.position.z < -groundHalfHeight) {
+      target.position.z = -groundHalfHeight;
+    }
   }
 
   private initHandlers() {
@@ -98,25 +139,25 @@ export class Camera {
 
     if (y <= CANVAS_PADDING_PX) {
       this.movementVector.addInPlace(
-        new Vector3(directionVectorX, 0, directionVectorZ),
+        new Vector3(directionVectorX, 0, directionVectorZ)
       );
     }
 
     if (y >= this.canvas.height - CANVAS_PADDING_PX) {
       this.movementVector.addInPlace(
-        new Vector3(-directionVectorX, 0, -directionVectorZ),
+        new Vector3(-directionVectorX, 0, -directionVectorZ)
       );
     }
 
     if (x <= CANVAS_PADDING_PX) {
       this.movementVector.addInPlace(
-        new Vector3(-directionVectorZ, 0, directionVectorX),
+        new Vector3(-directionVectorZ, 0, directionVectorX)
       );
     }
 
     if (x >= this.canvas.width - CANVAS_PADDING_PX) {
       this.movementVector.addInPlace(
-        new Vector3(directionVectorZ, 0, -directionVectorX),
+        new Vector3(directionVectorZ, 0, -directionVectorX)
       );
     }
 
@@ -124,45 +165,42 @@ export class Camera {
   };
 
   sceneTickHandler = () => {
-    const { scene, target, movementVector } = this;
-    if (!movementVector.length() || !scene.deltaTime) {
-      return;
-    }
-    const speed = (scene.deltaTime / 1000) * 4;
-    const groundHalfWidth = GROUND_WIDTH / 2;
-    const groundHalfHeight = GROUND_HEIGHT / 2;
-    target.position.addInPlace(movementVector.scale(speed));
-
-    if (target.position.x > groundHalfWidth) {
-      target.position.x = groundHalfWidth;
-    }
-
-    if (target.position.x < -groundHalfWidth) {
-      target.position.x = -groundHalfWidth;
-    }
-
-    if (target.position.z > groundHalfHeight) {
-      target.position.z = groundHalfHeight;
-    }
-
-    if (target.position.z < -groundHalfHeight) {
-      target.position.z = -groundHalfHeight;
-    }
+    this.refreshTargetPositionByMovementVector();
   };
 
   keyboardHandler = (keyboardInfo: KeyboardInfo) => {
     if (
       keyboardInfo.type === KeyboardEventTypes.KEYDOWN &&
-      keyboardInfo.event.code === "KeyE"
+      keyboardInfo.event.code === "KeyE" &&
+      this.rotationAnimationStartTimeMs === undefined
     ) {
-      this.rotationGradationIndex =
-        (this.rotationGradationIndex + 1) % CAMERA_ROTATION_GRADATIONS.length;
+      this.rotationAnimationStartTimeMs = Date.now();
     }
   };
 
   rotationHandler = () => {
-    const { camera, rotationGradationIndex } = this;
-    camera.alpha = CAMERA_ROTATION_GRADATIONS[rotationGradationIndex];
-    camera.beta = BETA_ROTATION_ANGLE;
+    const { camera, rotationGradationIndex, rotationAnimationStartTimeMs } =
+      this;
+
+    if (rotationAnimationStartTimeMs === undefined) {
+      camera.alpha = CAMERA_ROTATION_GRADATIONS[rotationGradationIndex];
+      camera.beta = BETA_ROTATION_ANGLE;
+      return;
+    }
+
+    const animationProgressMs = Date.now() - rotationAnimationStartTimeMs;
+    const nextRotationGradationIndex =
+      (rotationGradationIndex + 1) % CAMERA_ROTATION_GRADATIONS.length;
+
+    if (animationProgressMs >= ROTATION_ANIMATION_DURATION_MS) {
+      this.rotationAnimationStartTimeMs = undefined;
+      this.rotationGradationIndex = nextRotationGradationIndex;
+      return;
+    }
+
+    camera.alpha =
+      (ALPHA_DELTA / ROTATION_ANIMATION_DURATION_MS) *
+        (animationProgressMs - rotationAnimationStartTimeMs) +
+      CAMERA_ROTATION_GRADATIONS[rotationGradationIndex];
   };
 }
--- src/components/Game/engine/Camera/constants.ts
+++ src/components/Game/engine/Camera/constants.ts
@@ -1,18 +1,21 @@
-const DEFAULT_ROTATION_ANGLE = 1.5 * Math.PI;
 export const CAMERA_ROTATION_GRADATIONS = (() => {
+  const DEFAULT_ROTATION_ANGLE = 1.5 * Math.PI;
   const NUMBER_OF_CAMERA_ROTATION_GRADATIONS = 4; // Целое число, не меньшее двух.
+
   const ROTATION_DELTA = (2 * Math.PI) / NUMBER_OF_CAMERA_ROTATION_GRADATIONS;
   const CAMERA_ROTATION_GRADATIONS: number[] = [];
   for (let i = 0; i < NUMBER_OF_CAMERA_ROTATION_GRADATIONS; i++) {
     CAMERA_ROTATION_GRADATIONS.push(
-      DEFAULT_ROTATION_ANGLE + i * ROTATION_DELTA,
+      DEFAULT_ROTATION_ANGLE + i * ROTATION_DELTA
     );
   }
   return CAMERA_ROTATION_GRADATIONS;
 })();
+export const ALPHA_DELTA = (2 * Math.PI) / CAMERA_ROTATION_GRADATIONS.length;
 export const BETA_ROTATION_ANGLE = Math.PI / 4; // Угол наклона ( = угол между плоскостью XZ и направлением камеры), допустимые значения от 0 до Math.PI / 2.
 export const MIN_RADIUS = 5; // Минимальное расстояние от камеры до цели.
 export const MAX_RADIUS = 15; // Максимальное расстояние от камеры до цели.
 export const DEFAULT_RADIUS = MIN_RADIUS + (MAX_RADIUS - MIN_RADIUS) / 2;
 export const SHOW_TARGET = true;
 export const CANVAS_PADDING_PX = 100;
+export const ROTATION_ANIMATION_DURATION_MS = 1000;
